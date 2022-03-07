483ae450aff17ec65367000e125565b694b6258f
rafakasi <rafaelrksilva@gmail.com>
2022-03-07 12:40:19 -0300

    add introdução a javascript

---
+++ bloco-04-intro-javascript/scripts.js
@@ -0,0 +1,7 @@
+const myName = 'Rafael';
+const birthCity = 'Ipatinga';
+let birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log (birthYear);
---
+++ bloco-04-intro-javascript/scriptsifelse.js
@@ -0,0 +1,12 @@
+const notaCandidato = 80;
+
+if (notaCandidato >= 80){
+    console.log('Aprovado(a)!')
+}
+else if (notaCandidato < 80 && >= 60){
+    console.log('Você está na nossa lista de espera')
+}
+else (notaCandidato < 60){
+    console.log('Você foi reprovado(a)!')
+}
+
