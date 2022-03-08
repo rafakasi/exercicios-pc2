0f4e4799bdebdc6092e195df3693a58d4d43bff2
rafakasi <rafaelrksilva@gmail.com>
2022-03-08 11:38:29 -0300

    exercicios for

---
+++ bloco-04-intro-javascript/js-arrays-loopfor/forexercice.js
@@ -0,0 +1,5 @@
+let number = 5;
+
+for(let index = 1; index <= 10; index++) {
+    console.log(number * index);
+}
---
+++ bloco-04-intro-javascript/js-arrays-loopfor/forexercice2.js
@@ -0,0 +1,6 @@
+let listNames = ['Rafael', 'Stefany', 'Marcelo', 'Mary', 'Mel', 'Apolo', 'Joaquim', 'Napoleão', 'Leoãozinho', 'Wolverine'];
+
+for (let index = 0; index < listNames.length; index+=1) {
+    let mensage = 'Boas vindas!, ' + listNames[index] + ' fique a vontade e aproveite a aventura.';
+    console.log(mensage)
+}
