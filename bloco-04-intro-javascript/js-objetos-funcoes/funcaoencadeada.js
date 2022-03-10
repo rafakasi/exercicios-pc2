88bba7f13a62d9c90e9aeae1248fcbc7c2109d6c
rafakasi <rafaelrksilva@gmail.com>
2022-03-10 11:05:18 -0300

    exercicios objetos e funções

---
+++ bloco-04-intro-javascript/js-objetos-funcoes/funcaoencadeada.js
@@ -0,0 +1,24 @@
+//Informações de forma encadeada
+
+let usuario = {
+    id: 99,
+    email: 'jakeperalta@gmail.com',
+    infoPessoal: {
+      nome: 'Jake',
+      sobrenome: 'Peralta',
+      endereco: {
+        rua: 'Smith Street',
+        bairro: 'Brooklyn',
+        cidade: 'Nova Iorque',
+        estado: 'Nova Iorque',
+      },
+    },
+  };
+  
+  console.log(usuario['id']); // 99
+  console.log(usuario.email); // jakeperalta@gmail.com
+  
+  console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+  console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+  //3 chaves, uma dentro da outra.
