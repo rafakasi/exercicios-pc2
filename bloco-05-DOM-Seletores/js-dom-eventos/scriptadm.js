19cd83dfa604bf97ebf4c6d51ff9ed934dbb85b9
rafakasi <rafaelrksilva@gmail.com>
2022-03-15 09:44:34 -0300

    js eventos

--- bloco-05-DOM-Seletores/js-dom-eventos/scriptadm.js
+++ bloco-05-DOM-Seletores/js-dom-eventos/scriptadm.js
@@ -5,6 +5,11 @@ document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow'
 document.querySelector('.emergency-tasks').style.backgroundColor = 'pink'
 
 function titulosH () {
+    // let captureH = document.querySelectorAll('h3');
+    
+    // for (h3 of captureH) {
+    // style.backgroundColor = 'black'
+    // }
     for (i = 0; i <= 3; i += 1) {
         document.querySelectorAll('h3')[i].style.backgroundColor = 'black'
     }
