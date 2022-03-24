4472bbf4ee3845b9fc0b458a5bbc154547487b4b
rafakasi <rafaelrksilva@gmail.com>
2022-03-24 16:14:48 -0300

    Exercicios do conteúdo encerrados!

---
+++ bloco-06-forms-flexbox-respon/forms/trybe-trip/script.js
@@ -0,0 +1,12 @@
+// const btnApagaTudo = document.querySelector('#apagar-tudo')
+// const forms = document.querySelector('#formu')
+
+// // Função para resetar o formulário
+// function reset() {
+//     forms.innerText = '';
+// }
+
+// // Escutador do botão de apagar tudo
+// btnApagaTudo.addEventListener('click', reset)
+
+// KKAKAKKA vacilei ein?!
