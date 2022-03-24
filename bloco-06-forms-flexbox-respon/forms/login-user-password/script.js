567d5eb79d7ae3b7a2d468ed00c57b76fa95bac1
rafakasi <rafaelrksilva@gmail.com>
2022-03-24 11:02:28 -0300

    tentativa de salvar a senha

---
+++ bloco-06-forms-flexbox-respon/forms/login-user-password/script.js
@@ -0,0 +1,15 @@
+// Variáveis Globais.
+const btnLembraSenha = document.getElementById('lembrar-senha')
+const senhasDigitadas = document.getElementById('form-login')
+
+// // Salvar no localStorage
+// function guardaSenha(){
+//     localStorage.setItem(
+//         'senhas-salvas',
+//         JSON.stringify(senhasDigitadas.innerHTML),
+//     );
+// }
+
+// // Escutador do lembrar senha.
+// btnLembraSenha.addEventListener('click', guardaSenha);
+
