755390654f30921d4280e160769c559454ae31c2
rafakasi <rafaelrksilva@gmail.com>
2022-05-06 22:47:19 -0300

    exercicio dia 02 pokedex

---
+++ modulo-frontend/bloco10-introducao/dia02/my-pokedex/src/Pokedex.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Pokemon from './Pokemon'
+
+class Pokedex extends Component {
+  render() {
+    const { pokemons } = this.props;
+
+
+    return(
+    <div className='pokedex'>
+      {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+    </div>);
+  }
+}
+
+export default Pokedex;
+
+/* Na linha 10 aconteceu um map para renderizar o componente pokemon, que recebe por props cada item do array */
---
+++ modulo-frontend/bloco10-introducao/dia02/my-pokedex/src/Pokemon.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Pokemon extends Component {
+    render() {
+   const { pokemon } = this.props;
+   const { name, type, averageWeight, image } = pokemon;
+
+        return(
+            <div className='pokemon'>
+                <div>
+                    <p>{ name }</p>
+                    <p>{ type }</p>
+                    <p>{ `Average Weight boladão: ${averageWeight.value} ${averageWeight.measurementUnit}` }</p>
+                </div>
+                <img src={ image } alt={`${name} evaporou`} />
+            </div>
+        )
+    }
+}
+
+Pokemon.propTypes = {
+    pokemon: PropTypes.shape({
+      name: PropTypes.string,
+      type: PropTypes.string,
+      averageWeight: PropTypes.shape({
+        measurementUnit: PropTypes.string,
+        value: PropTypes.number,
+      }),
+      image: PropTypes.string,
+    }).isRequired,
+  };
+  
+
+export default Pokemon;
